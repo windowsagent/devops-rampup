146057ece7c5fc42116713755cd572f910cfb20c
windowsagent <juanjphc@gmail.com>
2024-04-17 15:31:43 -0600

    Change release to release_year

--- moviedb/schemas/movie_db/movies.sql
+++ moviedb/schemas/movie_db/movies.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `movies` (
   `title` varchar(250) NOT NULL,
-  `release` varchar(250) DEFAULT NULL,
+  `release_year` varchar(250) DEFAULT NULL,
   `score` int DEFAULT NULL,
   `reviewer` varchar(250) DEFAULT NULL,
   `publication` varchar(250) DEFAULT NULL,
