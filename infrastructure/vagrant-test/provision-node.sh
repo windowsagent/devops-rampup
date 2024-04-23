bc0cc9bd88ff9d4b70c31dbc4310afde31921602
windowsagent <juanjphc@gmail.com>
2024-04-23 14:26:35 -0600

    Add ansible playbook for k8s, vagrant test grounds

---
+++ infrastructure/vagrant-test/provision-node.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Configure SSH Daemon
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config && systemctl restart sshd
+
+# Install Ansible
+sudo apt update && sudo apt install ansible -y
+
+# Clone repo
+git clone "https://github.com/windowsagent/devops-rampup/"
