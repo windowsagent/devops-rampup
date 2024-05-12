69b06539ab74f9c4b7b0748cd1895149e1f14032
windowsagent <juanjphc@gmail.com>
2024-05-12 14:53:38 -0600

    Fix conflict with RDS tmp password

--- infrastructure/terraform/rds/main.tf
+++ infrastructure/terraform/rds/main.tf
@@ -12,7 +12,6 @@ resource "aws_db_instance" "movie_rds_instance" {
   engine                     = "mariadb"
   engine_version             = "10.11.6"
   instance_class             = "db.t4g.micro"
-  manage_master_user_password = "false"
   db_subnet_group_name       = aws_db_subnet_group.private_subnet.name
   vpc_security_group_ids     = [
     var.vpc_security_group_id
